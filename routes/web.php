49ce77b9f2aa184e920db500249a33d1f9da01f8
tacsteer <kirillshian@gmail.com>
2024-06-12 22:40:03 +0300

    Add some ext

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/{name}/{age}', function ($name,$age) {
+Route::get('/{name}/{age}', function ($name,$age)
+{
     return view('welcome', ['name' => $name, 'age' => $age]);
 });
+
+Route::get('/john/{age}', [\App\Http\Controllers\ValidAge::class, ''])
+    ->where('age', '[0-9]+'); // Ограничение параметра age только числовыми значениями
 
 
