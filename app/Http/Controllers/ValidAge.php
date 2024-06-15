b0a1f7bceaa450658b51369fbfe8ad6b90f3a8d1
Tacsteer <kirillshian@gmail.com>
2024-06-15 09:30:49 +0300

    Fixed Route ValidAge in web.php

--- app/Http/Controllers/ValidAge.php
+++ app/Http/Controllers/ValidAge.php
@@ -3,8 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class ValidAge extends Controller
 {
-    //
+    public function ageCheck($age)
+    {
+        if (!is_numeric($age)) {
+            //Вернуть статус 404, если возраст не является числом
+            return response()->json(['error' => 'Invalid age'], Response::HTTP_NOT_FOUND);
+        }
+        return response()->json(['age' => $age]);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ValidAge;
 
 Route::get('/{name}/{age}', function ($name,$age)
 {
     return view('welcome', ['name' => $name, 'age' => $age]);
 });
 
-Route::get('/john/{age}', [\App\Http\Controllers\ValidAge::class, ''])
+Route::get('/john/{age}', [\App\Http\Controllers\ValidAge::class, 'ageCheck'])
     ->where('age', '[0-9]+'); // Ограничение параметра age только числовыми значениями
 
 
